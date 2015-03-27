b9e7db6074d07de8cbe63bb7b7a05fb02f5e2892
ceebo <chricainogithub@gmail.com>
2015-03-27 16:11:36 +0000

    Confirm the tentative p01038's

--- get_area.py
+++ get_area.py
@@ -118,7 +118,7 @@ for filename in listdir("confirmed"):
 
 stats = {}
 
-for period in guns: #sorted(guns, key=guns.get, reverse=True):
+for period in guns:#sorted(guns, key=guns.get, reverse=True):
     if period < 1000:
         gun_type = guns[period][1].split("_")[0]
         stats[gun_type] = stats.get(gun_type, 0) + 1
